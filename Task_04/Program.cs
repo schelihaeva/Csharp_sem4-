72d2fb8a7e167a1a6a989820fb90ce0a09130414
shelihaeva <smelkovagolodnova@yandex.ru>
2023-10-17 21:50:20 +0400

    решение с помощью функций

---
+++ Task_04/Program.cs
@@ -0,0 +1,20 @@
+﻿ int FindLen(int N)
+ {
+   int Len = 0;
+     while (N > 0)
+   {
+        N /=10;
+        Len++;
+     }
+    return Len;  //возвращаем длинну
+ }
+
+int ReadInt(string text)  //функция будет внутрь себя принимать некторый текст
+{
+    System.Console.WriteLine(text);  //будем выводить текст
+    return Convert.ToInt32(Console.ReadLine()); //примаем какой то результат/переменную,
+    //возвращаем результат/переменную
+}
+
+int number = ReadInt("введите число ");
+System.Console.WriteLine(FindLen(number));
