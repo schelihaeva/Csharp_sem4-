c1c083ca47c8219d41def98da4cc566e449a66b7
shelihaeva <smelkovagolodnova@yandex.ru>
2023-10-18 13:26:43 +0400

    задача

--- Task_05/Program.cs
+++ Task_05/Program.cs
@@ -1,3 +1,21 @@
 ﻿// напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
 // 4 -> 24
 // 5 ->120
+
+int num (String N)
+{
+    System.Console.WriteLine(N);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Result(int N)
+{
+int mult = 1;
+for(int i = 2; i <=N; i++)
+{
+   mult *= i; // res*1, res*2  и т.д.до N
+}
+return mult;
+}
+//.................
+int number = num ("введите число ");
+System.Console.WriteLine(Result(number));
